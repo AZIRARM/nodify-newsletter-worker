92e7cd0473afab330e4f6c1b6fe70d386d0acbf7
AZIRAR Mhamed (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDSP IDFCE) <7812170p@commun.ad.sncf.fr>
2026-05-16 11:51:21 +0200

    migratin to postgres sql done

--- src/main/java/com/nodify/newsletter/model/UserNewsletterStatus.java
+++ src/main/java/com/nodify/newsletter/model/UserNewsletterStatus.java
@@ -1,15 +1,7 @@
 package com.nodify.newsletter.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
-
 import java.time.LocalDateTime;
 
 @Entity
@@ -19,13 +11,17 @@ public class UserNewsletterStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
     private User user;
-    @ManyToOne(cascade = CascadeType.ALL)
+
+    @ManyToOne
     @JoinColumn(name = "campaign_id")
     private Campaign campaign;
+
     @ManyToOne
     private Newsletter newsletter;
+
     private LocalDateTime sentAt;
     private LocalDateTime openedAt;
     private Boolean opened = false;
